fa0c7b69fc86a78d37e14cab11a29f1f128b6c92
Iso-latte <trujillo.joaquin101@gmail.com>
2024-03-28 15:03:00 -0600

    changed x-axis for stream

--- eatsafe/src/components/WebcamCapture.tsx
+++ eatsafe/src/components/WebcamCapture.tsx
@@ -50,11 +50,12 @@ const WebcamCapture: React.FC<WebcamCaptureProps> = ({ onCapture }) => {
     }
   };
 //USED IMAGE UPLOAD BUTTON on take picture
+//Take out the , transform: 'scaleX(-1)' to invert the stream
   return (
     <div>
-      {isVideoVisible && <video ref={videoRef} autoPlay style={{ width: '100%' }}></video>}
+      {isVideoVisible && <video ref={videoRef} autoPlay style={{ width: '95%', transform: 'scaleX(-1)' }}></video>}
       <button className="image-upload-button" onClick={takePicture}>Take Picture</button>
-      {imageSrc && <img src={imageSrc} alt="Captured" style={{ width: '100%' }} />}
+      {imageSrc && <img src={imageSrc} alt="Captured" style={{ width: '95%' }} />}
     </div>
   );
 };
