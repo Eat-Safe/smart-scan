1d269130ae5aaf98e47a1a9d1285819bc6510eb3
Haimei <130692101+Haimei-Chen@users.noreply.github.com>
2024-03-29 11:59:46 +1000

    fix detect xxx-free
    fix detect xxx-free

--- eatsafe/src/App.tsx
+++ eatsafe/src/App.tsx
@@ -19,6 +19,7 @@ function App() {
   const [ocrText, setOcrText] = useState('');
   const [isProcessing, setIsProcessing] = useState(false);
   const [allergens, setAllergens] = useState<string[]>([]);
+  const [hasChecked, setHasChecked] = useState(false);
 
   const handleImageChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     if (event.target.files && event.target.files[0]) {
@@ -64,9 +65,29 @@ function App() {
   };
 
   const detectAllergens = (text: string) => {
-    const detectedAllergens = allergensList.filter(allergen => text.toLowerCase().includes(allergen));
-    setAllergens(detectedAllergens);
+    const textLowercased = text.toLowerCase();
+    const freeRegex = /\b(\w+[- ]?free)\b/g; // 匹配 'xxx-free' 或 'xxx free'
+    const freeAllergens = textLowercased.match(freeRegex) || [];
+    const allergenFreeList = freeAllergens.map(allergenFree =>
+      allergenFree.replace(/[- ]?free/, '').trim()
+    );
+  
+    // 检测文本中的过敏原
+    const detectedAllergens = allergensList.filter(allergen => {
+      const allergenLowercased = allergen.toLowerCase();
+      // 如果在'allergen-free'列表中，跳过
+      if (allergenFreeList.includes(allergenLowercased)) {
+        return false;
+      }
+      // 检测文本是否含有过敏原
+      return textLowercased.includes(allergenLowercased);
+    });
+  
+    setAllergens(detectedAllergens); // 更新检测到的过敏原状态
+    setHasChecked(true);
   };
+  
+  
 
   return (
     <div className="App">
@@ -76,6 +97,9 @@ function App() {
       {/* Removed the button as extractTextFromImage is now called automatically */}
       {isProcessing ? <p>Extracting...</p> : <TextDisplay text={ocrText} />}
       <AllergensDisplay allergens={allergens} />
+      {hasChecked && allergens.length === 0 && (
+      <p className="allergen-status-safe">Safe food, no potential allergens detected.</p>
+      )}
     </div>
   );
 }
